5bfc8ba16f03ad7ab4b734bf91f0eb8d90e606c7
sanchetanparmar <sanchetanparmar@yahoo.com>
2020-06-22 21:18:15 +0530

    Create vars.tf

---
+++ vars.tf
@@ -0,0 +1,13 @@
+variable "subscription_id" {
+  default = ""
+}
+variable "client_id" {
+  default = ""
+}
+variable "client_secret" {
+  default = ""
+}
+
+variable "tenant_id" {
+  default = ""
+}
